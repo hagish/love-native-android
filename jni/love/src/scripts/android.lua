b8f07686835d03b643f8a2bb7310854661e5908a
Sebastian Dorda <hagish@schattenkind.net>
2012-05-20 16:19:16 +0200

    screen size changes start to work

--- jni/love/src/scripts/android.lua
+++ jni/love/src/scripts/android.lua
@@ -1,5 +1,6 @@
 -- android glue code
 
 function android_onScreenSizeChanged(width, height)
-	
+	print("android_onScreenSizeChanged", width, height)
+	love.graphics.setMode(width, height, true, 0)
 end
